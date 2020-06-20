1faa2be5504811f00eafcdcd0ac3ca786ff72950
Irene Scott <irenejinscott@gmail.com>
2020-06-20 00:45:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,8 @@ class App < Sinatra::Base
   post '/teams' do
     @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
 
-    members = params[:team][:members]
-binding.pry
+    members = params[:team], params[:members]
+
     members.each do |member_params|
       SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
     end

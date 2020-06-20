5a62c49455da1b45aec898475a80975b33344799
Irene Scott <irenejinscott@gmail.com>
2020-06-20 00:46:58 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,10 @@ class App < Sinatra::Base
     end
     @super_heroes = SuperHero.all
 
+    post '/teams' do
+          @team = Team.new({name: params[:team][:name], motto: params[:team][:motto]})
+          params[:team][:heroes].each {|hero| Superhero.new(hero)}
+          @heroes=Hero.all
     erb :team
   end
 end
